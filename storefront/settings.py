d8967476f70e075485cf8671139598b487d9a286
Ahmed El Nashar <a7medelnashar@hotmail.com>
2022-05-28 16:58:36 +0200

    created store and tags apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar'
+    'debug_toolbar',
+    'store',
+    'tags'
 ]
 
 MIDDLEWARE = [
